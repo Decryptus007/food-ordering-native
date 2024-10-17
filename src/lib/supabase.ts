40e46de88495d5fd00a591b55d5dc0b5c6d41518
decryptus007 <domnick080@outlook.com>
2024-10-18 00:53:08 +0100

    typescript added

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import "react-native-url-polyfill/auto";
 import * as SecureStore from "expo-secure-store";
 import { createClient } from "@supabase/supabase-js";
+import { Database } from "../database.types";
 
 const ExpoSecureStoreAdapter = {
   getItem: (key: string) => {
@@ -18,7 +19,7 @@ const supabaseUrl = "https://dwysuhhvnlruiqnjprql.supabase.co";
 const supabaseAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3eXN1aGh2bmxydWlxbmpwcnFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg0ODAyODYsImV4cCI6MjA0NDA1NjI4Nn0.e-hYTDmcdflqxYwTLnxiEKfWa44rcx0GNtJ072phvXM";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: ExpoSecureStoreAdapter as any,
     autoRefreshToken: true,
--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -7,6 +7,7 @@ import {
 } from "react";
 import { supabase } from "../lib/supabase";
 import { Session } from "@supabase/supabase-js";
+import { Profile } from "../types";
 
 type AuthData = {
   session: Session | null;
@@ -24,7 +25,7 @@ const AuthContext = createContext<AuthData>({
 
 export default function AuthProvider({ children }: PropsWithChildren) {
   const [session, setSession] = useState<Session | null>(null);
-  const [profile, setProfile] = useState(null);
+  const [profile, setProfile] = useState<Profile | null>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
